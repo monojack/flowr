8f32bcbab55ece52bb122fffb43bdcb712396c22
Ionut Achim <achimvionut@gmail.com>
2018-06-28 02:10:06 +0300

    (Throttle): proper implementation and propType definitions

--- src/Throttle.js
+++ src/Throttle.js
@@ -1,21 +1,63 @@
 import { Component, } from 'react'
+import { number, node, } from 'prop-types'
+
+/**
+ * props:       a--b--c--d--e--f------------g--h--j---------k|
+ *                                  time: 1000
+ * isPaused:    F-------------------------T-F----------------|
+ * hasNewProps: TF-T------F-T-------F-------TF-T------F-----T|
+ * forceUpdate: x---------x---------x-------x---------x-----x|
+ * render:      a---------d---------f-------g---------j-----k|
+ */
 
 class Throttle extends Component {
-  componentDidMount () {
-    this.clearInterval()
+  static propTypes = {
+    time: number,
+    children: node,
+  }
 
-    const { time, } = this.props
-    this.interval = setInterval(() => {
-      this.forceUpdate()
+  componentDidMount () {
+    this.hasNewProps = false
+    this.startSampling(this.props)
+  }
+
+  componentWillReceiveProps ({ time, }) {
+    this.clearTimeout()
+    this.hasNewProps = true
+
+    this.timeout = setTimeout(() => {
+      this.clearInterval(this.interval)
+      this.isPaused = true
     }, time)
+
+    if (this.isPaused) {
+      this.forceUpdate()
+      this.startSampling({ time, })
+    }
   }
 
   shouldComponentUpdate () {
     return false
   }
 
+  componentDidUpdate () {
+    this.hasNewProps = false
+  }
+
   componentWillUnmount () {
     this.clearInterval()
+    this.clearTimeout()
+  }
+
+  startSampling = ({ time, }) => {
+    this.isPaused = false
+    this.interval = setInterval(() => {
+      this.hasNewProps && this.forceUpdate()
+    }, time)
+  }
+
+  clearTimeout = () => {
+    this.timeout && clearTimeout(this.timeout)
   }
 
   clearInterval = () => {
